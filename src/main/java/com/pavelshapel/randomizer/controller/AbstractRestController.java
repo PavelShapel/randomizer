1c1a18b099ccb67ea99f643c1497886c9ffe70c2
pavelshapel <pavel.shapel@gmail.com>
2021-01-14 20:32:36 +0300

    refactor controllers - replace all methods to AbstractRestController.java

--- src/main/java/com/pavelshapel/randomizer/controller/AbstractRestController.java
+++ src/main/java/com/pavelshapel/randomizer/controller/AbstractRestController.java
@@ -1,32 +1,39 @@
 package com.pavelshapel.randomizer.controller;
 
+import com.pavelshapel.randomizer.aop.LogMethodResult;
 import com.pavelshapel.randomizer.entity.RandomEntity;
-import com.pavelshapel.randomizer.service.Randomizer;
-import org.apache.commons.lang3.Range;
+import com.pavelshapel.randomizer.service.randomizer.Randomizer;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+import java.util.List;
 
 public abstract class AbstractRestController<T> {
     public static final String PATH_GENERIC_RANGE = "/{min:[\\d]+}/{max:[\\d]+}";
+    public static final String PATH_LIST = "/list";
 
     @Autowired
     private Randomizer<T> randomizer;
 
-    protected RandomEntity<T> getRandomEntity() {
-        final T value = randomizer.randomize();
-        final Class<?> type = randomizer.getGenericParameterClass();
-
-        return RandomEntity.<T>builder().value(value).type(type).build();
+    @LogMethodResult
+    @GetMapping
+    protected ResponseEntity<RandomEntity<T>> getRandomEntity() {
+        return ResponseEntity.ok(randomizer.getRandomEntity());
     }
 
-    protected RandomEntity<T> getRandomEntity(long min, long max) {
-        final T value = randomizer.randomize(Range.between(min, max));
-        final Class<?> type = randomizer.getGenericParameterClass();
-
-        return RandomEntity.<T>builder().value(value).type(type).build();
+    @LogMethodResult
+    @GetMapping(PATH_GENERIC_RANGE)
+    protected ResponseEntity<RandomEntity<T>> getRandomEntity(
+            @PathVariable long min,
+            @PathVariable long max) {
+        return ResponseEntity.ok(randomizer.getRandomEntity(min, max));
     }
 
-    protected abstract ResponseEntity<RandomEntity<T>> randomize();
-
-    protected abstract ResponseEntity<RandomEntity<T>> randomize(long min, long max);
+    @LogMethodResult
+    @GetMapping(PATH_LIST)
+    protected ResponseEntity<List<RandomEntity<T>>> getRandomEntityList() {
+        return ResponseEntity.ok(randomizer.getRandomEntityList());
+    }
 }
--- src/main/java/com/pavelshapel/randomizer/controller/LongRestController.java
+++ src/main/java/com/pavelshapel/randomizer/controller/LongRestController.java
@@ -1,31 +1,9 @@
 package com.pavelshapel.randomizer.controller;
 
-import com.pavelshapel.randomizer.aop.LogMethodResult;
-import com.pavelshapel.randomizer.entity.RandomEntity;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/")
+@RequestMapping("/long")
 public class LongRestController extends AbstractRestController<Long> {
-    public static final String PATH="/long";
-
-    @LogMethodResult
-    @GetMapping(PATH)
-    @Override
-    protected ResponseEntity<RandomEntity<Long>> randomize() {
-        return ResponseEntity.ok(getRandomEntity());
-    }
-
-    @LogMethodResult
-    @GetMapping(PATH + PATH_GENERIC_RANGE)
-    @Override
-    protected ResponseEntity<RandomEntity<Long>> randomize(
-            @PathVariable long min,
-            @PathVariable long max) {
-        return ResponseEntity.ok(getRandomEntity(min, max));
-    }
 }
