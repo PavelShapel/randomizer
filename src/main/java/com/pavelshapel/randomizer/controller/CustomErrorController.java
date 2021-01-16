15ef9c5a024ba90ac5156ed8aa5c10d4bdc02d22
pavelshapel <pavel.shapel@gmail.com>
2021-01-16 14:37:45 +0300

    refactor - add CustomErrorController.java

---
+++ src/main/java/com/pavelshapel/randomizer/controller/CustomErrorController.java
@@ -0,0 +1,20 @@
+package com.pavelshapel.randomizer.controller;
+
+import org.springframework.boot.web.servlet.error.ErrorController;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.view.RedirectView;
+
+@RestController
+public class CustomErrorController implements ErrorController {
+
+    @RequestMapping("/error")
+    public RedirectView handleError() {
+        return new RedirectView("/actuator/mappings");
+    }
+
+    @Override
+    public String getErrorPath() {
+        return "/error";
+    }
+}
