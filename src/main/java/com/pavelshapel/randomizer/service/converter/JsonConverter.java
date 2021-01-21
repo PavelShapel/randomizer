b750b79836e1e1adf03e60bcf75b6c0b2a5de0aa
pavelshapel <pavel.shapel@gmail.com>
2021-01-21 17:56:31 +0300

    refactor - delete all jackson properties from application.yml, add new bean objectMapper

--- src/main/java/com/pavelshapel/randomizer/service/converter/JsonConverter.java
+++ src/main/java/com/pavelshapel/randomizer/service/converter/JsonConverter.java
@@ -1,44 +1,34 @@
 package com.pavelshapel.randomizer.service.converter;
 
 import com.fasterxml.jackson.core.type.TypeReference;
-import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.RequiredArgsConstructor;
 import lombok.SneakyThrows;
 import org.springframework.stereotype.Service;
 
-import java.text.SimpleDateFormat;
 import java.util.Map;
 
 @Service
+@RequiredArgsConstructor
 public class JsonConverter {
+    private ObjectMapper objectMapper;
+
     @SneakyThrows
     public String pojoToJson(Object object) {
-        return getObjectMapper().writeValueAsString(object);
+        return objectMapper.writeValueAsString(object);
     }
 
     @SneakyThrows
     public <T> T jsonToPojo(String json, Class<T> targetClass) {
-        return getObjectMapper().readValue(json, targetClass);
+        return objectMapper.readValue(json, targetClass);
     }
 
     public Map<String, Object> pojoToMap(Object object) {
-        return getObjectMapper().convertValue(object, new TypeReference<Map<String, Object>>() {
+        return objectMapper.convertValue(object, new TypeReference<Map<String, Object>>() {
         });
     }
 
     public <T> T mapToPojo(Map<String, Object> map, Class<T> targetClass) {
-        return getObjectMapper().convertValue(map, targetClass);
+        return objectMapper.convertValue(map, targetClass);
     }
-
-    private ObjectMapper getObjectMapper() {
-        ObjectMapper mapper = new ObjectMapper();
-
-        mapper.enable(DeserializationFeature.ACCEPT_EMPTY_STRING_AS_NULL_OBJECT);
-        mapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
-        mapper.setDateFormat(new SimpleDateFormat("yyyy-MM-dd HH:mm:ss"));
-
-        return mapper;
-    }
-
-
 }
