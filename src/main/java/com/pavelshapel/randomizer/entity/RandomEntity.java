992e525eb850f9f2d075893bea0352638bb661c0
pavelshapel <pavel.shapel@gmail.com>
2021-01-22 09:23:01 +0300

    refactor

--- src/main/java/com/pavelshapel/randomizer/entity/RandomEntity.java
+++ src/main/java/com/pavelshapel/randomizer/entity/RandomEntity.java
@@ -1,14 +1,8 @@
 package com.pavelshapel.randomizer.entity;
 
-import lombok.*;
-import lombok.experimental.FieldDefaults;
+import lombok.Value;
 
-@Getter
-@EqualsAndHashCode
-@ToString
-@FieldDefaults(level = AccessLevel.PRIVATE)
-@AllArgsConstructor
-@NoArgsConstructor
+@Value
 public class RandomEntity<T> {
     T value;
     Class<T> type;
