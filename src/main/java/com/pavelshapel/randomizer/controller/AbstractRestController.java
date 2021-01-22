9d2e662ef4ccb514d5a623c1e07ca2235a7e9df6
pavelshapel <pavel.shapel@gmail.com>
2021-01-22 17:30:43 +0300

    refactor

--- src/main/java/com/pavelshapel/randomizer/controller/AbstractRestController.java
+++ src/main/java/com/pavelshapel/randomizer/controller/AbstractRestController.java
@@ -2,25 +2,26 @@ package com.pavelshapel.randomizer.controller;
 
 import com.pavelshapel.randomizer.aop.LogMethodResult;
 import com.pavelshapel.randomizer.entity.RandomEntity;
-import com.pavelshapel.randomizer.service.randomizer.Randomizer;
+import com.pavelshapel.randomizer.service.randomizer.randomentity.RandomEntityRandomizer;
+import org.apache.commons.lang3.Range;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-import java.util.List;
+import java.util.Collection;
 
 public abstract class AbstractRestController<T> {
     public static final String PATH_GENERIC_RANGE = "/{min:[\\d]+}/{max:[\\d]+}";
-    public static final String PATH_LIST = "/list";
+    public static final String PATH_COLLECTION = "/collection";
 
     @Autowired
-    private Randomizer<T> randomizer;
+    private RandomEntityRandomizer<T> randomEntityRandomizer;
 
     @LogMethodResult
     @GetMapping
     protected ResponseEntity<RandomEntity<T>> getRandomEntity() {
-        return ResponseEntity.ok(randomizer.getRandomEntity());
+        return ResponseEntity.ok(randomEntityRandomizer.randomize());
     }
 
     @LogMethodResult
@@ -28,12 +29,12 @@ public abstract class AbstractRestController<T> {
     protected ResponseEntity<RandomEntity<T>> getRandomEntity(
             @PathVariable long min,
             @PathVariable long max) {
-        return ResponseEntity.ok(randomizer.getRandomEntity(min, max));
+        return ResponseEntity.ok(randomEntityRandomizer.randomize(Range.between(min, max)));
     }
 
     @LogMethodResult
-    @GetMapping(PATH_LIST)
-    protected ResponseEntity<List<RandomEntity<T>>> getRandomEntityList() {
-        return ResponseEntity.ok(randomizer.getRandomEntityList());
+    @GetMapping(PATH_COLLECTION)
+    protected ResponseEntity<Collection<RandomEntity<T>>> getRandomEntityList() {
+        return ResponseEntity.ok(randomEntityRandomizer.randomizeCollection());
     }
 }
