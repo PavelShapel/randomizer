6ece4dcb254d187fe2cd1caf16320b071224c87b
pavelshapel <pavel.shapel@gmail.com>
2021-01-22 10:43:59 +0300

    refactor

--- src/main/java/com/pavelshapel/randomizer/service/converter/JsonConverter.java
+++ src/main/java/com/pavelshapel/randomizer/service/converter/JsonConverter.java
@@ -2,33 +2,64 @@ package com.pavelshapel.randomizer.service.converter;
 
 import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import lombok.RequiredArgsConstructor;
-import lombok.SneakyThrows;
+import lombok.AllArgsConstructor;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 import org.springframework.stereotype.Service;
 
 import java.util.Map;
+import java.util.Optional;
 
 @Service
-@RequiredArgsConstructor
+@AllArgsConstructor
 public class JsonConverter {
-    private ObjectMapper objectMapper;
+    private final ObjectMapper objectMapper;
 
-    @SneakyThrows
-    public String pojoToJson(Object object) {
-        return objectMapper.writeValueAsString(object);
+    public Optional<String> pojoToJson(Object object) {
+        try {
+            final String json = objectMapper.writeValueAsString(object);
+            return isValidJson(json) ? Optional.ofNullable(json) : Optional.empty();
+        } catch (Exception exception) {
+            return Optional.empty();
+        }
     }
 
-    @SneakyThrows
-    public <T> T jsonToPojo(String json, Class<T> targetClass) {
-        return objectMapper.readValue(json, targetClass);
+    public <T> Optional<T> jsonToPojo(String json, Class<T> targetClass) {
+        try {
+            return isValidJson(json) ? Optional.ofNullable(objectMapper.readValue(json, targetClass)) : Optional.empty();
+        } catch (Exception exception) {
+            return Optional.empty();
+        }
     }
 
-    public Map<String, Object> pojoToMap(Object object) {
-        return objectMapper.convertValue(object, new TypeReference<Map<String, Object>>() {
-        });
+    public Optional<Map<String, Object>> pojoToMap(Object object) {
+        try {
+            return Optional.ofNullable(objectMapper.convertValue(object, new TypeReference<Map<String, Object>>() {
+            }));
+        } catch (Exception exception) {
+            return Optional.empty();
+        }
     }
 
-    public <T> T mapToPojo(Map<String, Object> map, Class<T> targetClass) {
-        return objectMapper.convertValue(map, targetClass);
+    public <T> Optional<T> mapToPojo(Map<String, Object> map, Class<T> targetClass) {
+        try {
+            return Optional.ofNullable(objectMapper.convertValue(map, targetClass));
+        } catch (Exception exception) {
+            return Optional.empty();
+        }
+    }
+
+    public boolean isValidJson(String json) {
+        try {
+            new JSONObject(json);
+        } catch (JSONException externalException) {
+            try {
+                new JSONArray(json);
+            } catch (JSONException internalException) {
+                return false;
+            }
+        }
+        return true;
     }
 }
