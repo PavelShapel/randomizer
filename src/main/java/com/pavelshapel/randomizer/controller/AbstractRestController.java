4d327d989ff975a7a017d1a174d3d426684b3796
pavelshapel <pavel.shapel@gmail.com>
2021-01-24 21:45:06 +0300

    refactor

--- src/main/java/com/pavelshapel/randomizer/controller/AbstractRestController.java
+++ src/main/java/com/pavelshapel/randomizer/controller/AbstractRestController.java
@@ -5,6 +5,7 @@ import com.pavelshapel.randomizer.entity.RandomEntity;
 import com.pavelshapel.randomizer.service.Utilities;
 import com.pavelshapel.randomizer.service.randomizer.collection.CollectionRandomizer;
 import com.pavelshapel.randomizer.service.randomizer.primitive.PrimitiveRandomizer;
+import lombok.Getter;
 import org.apache.commons.lang3.Range;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -14,6 +15,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import java.util.Collection;
 import java.util.stream.Collectors;
 
+@Getter
 public abstract class AbstractRestController<T> {
     public static final String PATH_VALUE_RANGE = "/{minValue:[\\d]+}/{maxValue:[\\d]+}";
     public static final String PATH_SIZE_RANGE = "/{minSize:[\\d]+}/{maxSize:[\\d]+}";
@@ -35,7 +37,7 @@ public abstract class AbstractRestController<T> {
 
     @LogMethodResult(logLevel = "DEBUG")
     @GetMapping(PATH_VALUE_RANGE)
-    protected ResponseEntity<RandomEntity<T>> getRandomEntity(
+    protected ResponseEntity<RandomEntity<T>> get(
             @PathVariable long minValue,
             @PathVariable long maxValue) {
         final RandomEntity<T> randomEntity = getRandomEntity(primitiveRandomizer.randomize(Range.between(minValue, maxValue)));
@@ -44,7 +46,7 @@ public abstract class AbstractRestController<T> {
 
     @LogMethodResult(logLevel = "DEBUG")
     @GetMapping(PATH_COLLECTION)
-    protected ResponseEntity<Collection<RandomEntity<T>>> getRandomEntityCollection() {
+    protected ResponseEntity<Collection<RandomEntity<T>>> getCollection() {
         final Collection<RandomEntity<T>> randomEntities = collectionRandomizer.randomize().stream()
                 .map(this::getRandomEntity)
                 .collect(Collectors.toList());
@@ -53,7 +55,7 @@ public abstract class AbstractRestController<T> {
 
     @LogMethodResult(logLevel = "DEBUG")
     @GetMapping(PATH_COLLECTION + PATH_SIZE_RANGE)
-    protected ResponseEntity<Collection<RandomEntity<T>>> getRandomEntityCollectionBySize(
+    protected ResponseEntity<Collection<RandomEntity<T>>> getCollectionBySize(
             @PathVariable long minSize,
             @PathVariable long maxSize) {
         final Collection<RandomEntity<T>> randomEntities = collectionRandomizer.randomize(Range.between(minSize, maxSize)).stream()
@@ -64,7 +66,7 @@ public abstract class AbstractRestController<T> {
 
     @LogMethodResult(logLevel = "DEBUG")
     @GetMapping(PATH_VALUE_RANGE + PATH_COLLECTION)
-    protected ResponseEntity<Collection<RandomEntity<T>>> getRandomEntityCollectionByValue(
+    protected ResponseEntity<Collection<RandomEntity<T>>> getCollectionByValue(
             @PathVariable long minValue,
             @PathVariable long maxValue) {
         final Collection<RandomEntity<T>> randomEntities = collectionRandomizer.randomize(Range.between(minValue, maxValue), null).stream()
@@ -75,7 +77,7 @@ public abstract class AbstractRestController<T> {
 
     @LogMethodResult(logLevel = "DEBUG")
     @GetMapping(PATH_VALUE_RANGE + PATH_COLLECTION + PATH_SIZE_RANGE)
-    protected ResponseEntity<Collection<RandomEntity<T>>> getRandomEntityCollectionByValueSize(
+    protected ResponseEntity<Collection<RandomEntity<T>>> getCollectionByValueSize(
             @PathVariable long minValue,
             @PathVariable long maxValue,
             @PathVariable long minSize,
@@ -87,7 +89,7 @@ public abstract class AbstractRestController<T> {
         return ResponseEntity.ok(randomEntities);
     }
 
-    private RandomEntity<T> getRandomEntity(T value) {
+    protected RandomEntity<T> getRandomEntity(T value) {
         return new RandomEntity<>(value, getGenericParameterClass());
     }
 
--- src/main/java/com/pavelshapel/randomizer/controller/MapRestController.java
+++ src/main/java/com/pavelshapel/randomizer/controller/MapRestController.java
@@ -1,11 +1,20 @@
 package com.pavelshapel.randomizer.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.pavelshapel.randomizer.aop.LogMethodResult;
+import com.pavelshapel.randomizer.entity.RandomEntity;
+import com.pavelshapel.randomizer.service.randomizer.primitive.map.MapRandomizer;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.Map;
 
 @RestController
 @RequestMapping("/map")
 public class MapRestController extends AbstractRestController<Map<String, Object>> {
+    @LogMethodResult(logLevel = "DEBUG")
+    @GetMapping("/init")
+    public ResponseEntity<RandomEntity<Map<String, Object>>> getMap(@RequestBody Map<String, Object> map) {
+        final Map<String, Object> randomMap = ((MapRandomizer<Map<String, Object>>) getPrimitiveRandomizer()).randomize(map);
+        return ResponseEntity.ok(getRandomEntity(randomMap));
+    }
 }
