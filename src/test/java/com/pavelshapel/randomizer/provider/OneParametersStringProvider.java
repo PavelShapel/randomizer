ea36a9a76f33b5b54c021ab58c4b7036ffbb41aa
pavelshapel <pavel.shapel@gmail.com>
2021-01-28 09:10:34 +0300

    refactor - add new StringProvider.java for tests

---
+++ src/test/java/com/pavelshapel/randomizer/provider/OneParametersStringProvider.java
@@ -0,0 +1,7 @@
+package com.pavelshapel.randomizer.provider;
+
+public final class OneParametersStringProvider extends StringProvider {
+    public OneParametersStringProvider() {
+        super(1);
+    }
+}
