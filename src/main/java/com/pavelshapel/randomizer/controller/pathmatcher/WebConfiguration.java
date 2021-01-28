4a4998dedde3276ddbfad8f60441e4be8fbf4059
pavelshapel <pavel.shapel@gmail.com>
2021-01-28 10:02:19 +0300

    refactor

--- src/main/java/com/pavelshapel/randomizer/controller/pathmatcher/WebConfiguration.java
+++ src/main/java/com/pavelshapel/randomizer/controller/pathmatcher/WebConfiguration.java
@@ -1,25 +1,17 @@
-//package com.pavelshapel.randomizer.controller.pathmatcher;
-//
-//import org.springframework.context.annotation.Bean;
-//import org.springframework.context.annotation.Configuration;
-//import org.springframework.util.PathMatcher;
-//import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
-//import org.springframework.web.servlet.mvc.method.annotation.RequestMappingHandlerMapping;
-//
-//@Configuration
-//public class WebConfiguration extends WebMvcConfigurationSupport {
-//
-//    @Bean
-//    public PathMatcher pathMatcher() {
-//        return new CaseInsensitivePathMatcher();
-//    }
-//
-//    @Bean
-//    public RequestMappingHandlerMapping requestMappingHandlerMapping() {
-//        RequestMappingHandlerMapping handlerMapping = new RequestMappingHandlerMapping();
-//        handlerMapping.setOrder(0);
-//        handlerMapping.setInterceptors(getInterceptors());
-//        handlerMapping.setPathMatcher(pathMatcher());
-//        return handlerMapping;
-//    }
-//}
+package com.pavelshapel.randomizer.controller.pathmatcher;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.util.AntPathMatcher;
+import org.springframework.web.servlet.config.annotation.PathMatchConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
+
+@Configuration
+public class WebConfiguration extends WebMvcConfigurationSupport {
+
+    @Override
+    protected void configurePathMatch(PathMatchConfigurer configurer) {
+        AntPathMatcher matcher = new AntPathMatcher();
+        matcher.setCaseSensitive(false);
+        configurer.setPathMatcher(matcher);
+    }
+}
