7b2eb69984ecd650652bb859d99b8a2a06dbbcca
pavelshapel <pavel.shapel@gmail.com>
2021-01-28 09:10:58 +0300

    refactor

--- src/test/java/com/pavelshapel/randomizer/TaskTest.java
+++ src/test/java/com/pavelshapel/randomizer/TaskTest.java
@@ -1,18 +1,37 @@
 package com.pavelshapel.randomizer;//package com.pavelshapel.randomizer.service;
 
+import com.pavelshapel.randomizer.service.randomizer.array.LongArrayRandomizer;
+import com.pavelshapel.randomizer.service.randomizer.primitive.LongPrimitiveRandomizer;
+import com.pavelshapel.randomizer.service.randomizer.primitive.StringPrimitiveRandomizer;
 import lombok.AllArgsConstructor;
+import org.apache.commons.lang3.Range;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ContextConfiguration;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.function.Predicate;
 
-
+@SpringBootTest
+@ContextConfiguration(classes = {
+        StringPrimitiveRandomizer.class,
+        LongArrayRandomizer.class,
+        LongPrimitiveRandomizer.class
+})
 class TaskTest {
+    @Autowired
+    StringPrimitiveRandomizer stringPrimitiveRandomizer;
+    @Autowired
+    LongArrayRandomizer longArrayRandomizer;
+
     @Test
     void test() {
 
-        final List<Student> students = getStudents();
+        //final List<Student> students = getStudents();
+        final List<Student> students = getRandomStudents();
 
         printDelimiter();
         students.stream()
@@ -34,18 +53,29 @@ class TaskTest {
         System.out.println("********************************");
     }
 
+    private List<Student> getRandomStudents() {
+        final ArrayList<Student> students = new ArrayList<>();
+        for (int i = 0; i < 20; i++) {
+            final String randomName = stringPrimitiveRandomizer.randomize(Range.between(3L, 10L));
+            final Long[] randomMark = longArrayRandomizer.randomize(Range.between(6L, 10L), Range.between(1L, 5L));
+
+            students.add(new Student(randomName, Arrays.asList(randomMark.clone())));
+        }
+        return students;
+    }
+
     private List<Student> getStudents() {
         return Arrays.asList(
-                new Student("pavel1", Arrays.asList(8, 9, 10)),
-                new Student("pavel2", Arrays.asList(7, 8, 9, 10)),
-                new Student("pavel3", Arrays.asList(9, 10)),
-                new Student("pavel4", Arrays.asList(4, 5))
+                new Student("pavel1", Arrays.asList(8L, 9L, 10L)),
+                new Student("pavel2", Arrays.asList(7L, 8L, 9L, 10L)),
+                new Student("pavel3", Arrays.asList(9L, 10L)),
+                new Student("pavel4", Arrays.asList(4L, 5L))
         );
     }
 
     @AllArgsConstructor
     static class Student {
         String name;
-        List<Integer> marks;
+        List<Long> marks;
     }
 }
--- src/test/java/com/pavelshapel/randomizer/controller/AbstractRestControllerTest.java
+++ src/test/java/com/pavelshapel/randomizer/controller/AbstractRestControllerTest.java
@@ -5,6 +5,7 @@ import com.pavelshapel.randomizer.service.converter.JsonConverter;
 import com.pavelshapel.randomizer.service.randomizer.array.ArrayRandomizer;
 import com.pavelshapel.randomizer.service.randomizer.primitive.PrimitiveRandomizer;
 import lombok.AccessLevel;
+import lombok.Getter;
 import lombok.SneakyThrows;
 import lombok.experimental.FieldDefaults;
 import org.apache.commons.lang3.Range;
@@ -14,7 +15,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.context.annotation.Import;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
@@ -23,13 +23,12 @@ import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import java.util.Optional;
 
 import static com.pavelshapel.randomizer.entity.DefaultRanges.DEFAULT_POSITIVE_BYTE_RANGE;
-import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
-import static org.mockito.Mockito.*;
 
 @AutoConfigureMockMvc
 @Import({
         JsonConverter.class
 })
+@Getter
 @FieldDefaults(level = AccessLevel.PRIVATE)
 @ExtendWith(MockitoExtension.class)
 abstract class AbstractRestControllerTest<T> {
@@ -42,37 +41,6 @@ abstract class AbstractRestControllerTest<T> {
     @MockBean
     ArrayRandomizer<T> arrayRandomizer;
 
-    protected void get(T value) {
-        final RandomEntity<T> mockRandomEntity = createMockRandomEntity(value);
-        when(primitiveRandomizer.randomize()).thenReturn(mockRandomEntity.getValue());
-        when(primitiveRandomizer.getGenericClass()).thenReturn(mockRandomEntity.getType());
-
-        MvcResult mvcResult = getMvcResult("");
-
-        final RandomEntity<T> randomEntityFromResponse = getRandomEntityFromResponse(mvcResult);
-        int status = mvcResult.getResponse().getStatus();
-
-        assertThat(randomEntityFromResponse).isNotNull();
-        assertThat(status).isEqualTo(HttpStatus.OK.value());
-        verify(primitiveRandomizer, times(1)).randomize();
-    }
-
-    protected void getByRange(T value) {
-        final RandomEntity<T> mockRandomEntity = createMockRandomEntity(value);
-        final Range<Long> range = getRange();
-        when(primitiveRandomizer.randomize(range)).thenReturn(mockRandomEntity.getValue());
-        when(primitiveRandomizer.getGenericClass()).thenReturn(mockRandomEntity.getType());
-
-        MvcResult mvcResult = getMvcResult(getBoundedPath());
-
-        final RandomEntity<T> randomEntityFromResponse = getRandomEntityFromResponse(mvcResult);
-        int status = mvcResult.getResponse().getStatus();
-
-        assertThat(randomEntityFromResponse).isNotNull();
-        assertThat(status).isEqualTo(HttpStatus.OK.value());
-        verify(primitiveRandomizer, times(1)).randomize(range);
-    }
-
     @SneakyThrows
     protected MvcResult getMvcResult(String path) {
         return mockMvc
@@ -83,19 +51,19 @@ abstract class AbstractRestControllerTest<T> {
     }
 
     @SneakyThrows
-    protected RandomEntity<T> getRandomEntityFromResponse(MvcResult mvcResult) {
+    protected RandomEntity<T> getResponseBody(MvcResult mvcResult) {
         String content = mvcResult.getResponse().getContentAsString();
         final Optional<RandomEntity> optionalRandomEntity = jsonConverter.jsonToPojo(content, RandomEntity.class);
         return optionalRandomEntity.orElseThrow(IllegalArgumentException::new);
     }
 
-    private Range<Long> getRange() {
+    protected Range<Long> getRange() {
         return DEFAULT_POSITIVE_BYTE_RANGE.getValue();
     }
 
-    private String getBoundedPath() {
+    protected String getBoundedPath() {
         return String.format("/%d/%d", getRange().getMinimum(), getRange().getMaximum());
     }
 
-    protected abstract RandomEntity<T> createMockRandomEntity(T value);
+    protected abstract RandomEntity<T> createMockEntity(T value);
 }
