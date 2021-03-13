582d71fcea07d603f5b9f64b008c8d37f121e4dc
pavelshapel <pavel.shapel@gmail.com>
2021-03-13 16:01:16 +0300

    refactor

--- src/test/java/com/pavelshapel/randomizer/controller/RandomRestControllerTest.java
+++ src/test/java/com/pavelshapel/randomizer/controller/RandomRestControllerTest.java
@@ -2,7 +2,9 @@ package com.pavelshapel.randomizer.controller;
 
 import com.pavelshapel.random.spring.boot.starter.StarterAutoConfiguration;
 import com.pavelshapel.stream.spring.boot.starter.util.StreamUtils;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.http.HttpStatus;
@@ -11,14 +13,18 @@ import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
+import java.util.stream.Stream;
+
 import static com.pavelshapel.random.spring.boot.starter.StarterAutoConfiguration.*;
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.junit.jupiter.params.provider.Arguments.arguments;
 
 @WebMvcTest(properties = {
         PREFIX + "." + PROPERTY_NAME + "=" + TRUE
 },
         controllers = RandomRestController.class)
 @ContextConfiguration(classes = {
+        RandomRestController.class,
         StarterAutoConfiguration.class,
         StreamUtils.class
 })
@@ -26,11 +32,12 @@ class RandomRestControllerTest {
     @Autowired
     private MockMvc mockMvc;
 
-    @Test
-    void randomize_WithoutParams_ShouldReturnRandomizedValue() throws Exception {
+    @ParameterizedTest
+    @MethodSource("generateTypes")
+    void randomize_WithoutParams_ShouldReturnRandomizedValue(String type) throws Exception {
         MvcResult mvcResult = mockMvc
                 .perform(MockMvcRequestBuilders
-                        .get("/long"))
+                        .get("/{type}", type))
                 .andReturn();
 
         String responseBody = mvcResult.getResponse().getContentAsString();
@@ -38,5 +45,15 @@ class RandomRestControllerTest {
 
         assertThat(responseBody).isNotNull();
         assertThat(status).isEqualTo(HttpStatus.OK.value());
+    }
+
+    private static Stream<Arguments> generateTypes() {
+        return Stream.of(
+                arguments("long"),
+                arguments("boolean"),
+                arguments("string"),
+                arguments("date"),
+                arguments("double")
+        );
     }
 }
